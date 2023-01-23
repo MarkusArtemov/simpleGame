a639fe9a1532c00acc383c758b3dc81bc69b277b
MarkusArtemov <martemov16@gmail.com>
2023-01-23 09:48:18 +0100

    Restart Funktion hinzugefügt, Intervalle in einer Liste ausgelagert

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 const button = document.getElementById("click-button");
 const endGameMessage = document.querySelector(".endgameMessage");
+const restartButton = document.getElementById("restart-button");
 
+
+const intervals = [];
 
 const moveButton = () => {
-  let x = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
-  let y = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
+  const x = Math.floor(Math.random() * (window.innerWidth - button.offsetWidth));
+  const y = Math.floor(Math.random() * (window.innerHeight - button.offsetHeight));
   button.style.position = "absolute";
   button.style.left = x + "px";
   button.style.top = y + "px";
 }
-
-
-   
+ 
 const showResult = () => {
   endGameMessage.style.display = 'block';
+  restartButton.style.display = 'block';
   button.style.display = 'none';
-  clearInterval(gameIntervall);
+  clearInterval(gameInterval);
 }
 
-let gameIntervall = setInterval(moveButton, 1000);
+const restartGame = () => {
+  endGameMessage.style.display = 'none';
+  restartButton.style.display = 'none';
+  button.style.display = 'block';
+  setUpInterval();
+}
+
+const setUpInterval = () => {
+  intervals.forEach(interval => clearInterval(interval));
+  intervals.length = 0;
+  const gameInterval = setInterval(moveButton, 1000);
+  intervals.push(gameInterval);
+}
+
+
+const gameInterval = setInterval(moveButton, 1000);
+intervals.push(gameInterval);
 button.addEventListener("click",showResult);
+restartButton.addEventListener("click",restartGame);
